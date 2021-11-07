a0ee9effb0edc83af4caafd218b0fd2ead1b105b
unknown <hajaraswadkom@gmail.com>
2021-11-07 16:31:15 +0800

    login proses

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -6,43 +6,40 @@
           <CCardGroup>
             <CCard class="p-4">
               <CCardBody>
-                <CForm>
-                  <h1>Login</h1>
-                  <p class="text-medium-emphasis">Sign In to your account</p>
-                  <CInputGroup class="mb-3">
-                    <CInputGroupText>
-                      <CIcon icon="cil-user" />
-                    </CInputGroupText>
-                    <CFormInput
-                      placeholder="Username"
-                      autocomplete="username"
-                    />
-                  </CInputGroup>
-                  <CInputGroup class="mb-4">
-                    <CInputGroupText>
-                      <CIcon icon="cil-lock-locked" />
-                    </CInputGroupText>
-                    <CFormInput
-                      type="password"
-                      placeholder="Password"
-                      autocomplete="current-password"
-                    />
-                  </CInputGroup>
-                  <CRow>
-                    <CCol :xs="5">
-                      <CButton
-                        color="primary col"
-                        class="px-4"
-                        @click="
-                          () => {
-                            this.$router.push('dashboard')
-                          }
-                        "
-                        >Login</CButton
-                      >
-                    </CCol>
-                  </CRow>
-                </CForm>
+                <h1>Login</h1>
+                <p class="text-medium-emphasis">Sign In to your account</p>
+                <CInputGroup class="mb-3">
+                  <CInputGroupText>
+                    <CIcon icon="cil-user" />
+                  </CInputGroupText>
+                  <input
+                    id="username"
+                    v-model="form.username"
+                    class="form-control"
+                    required
+                    name="username"
+                  />
+                </CInputGroup>
+                <CInputGroup class="mb-4">
+                  <CInputGroupText>
+                    <CIcon icon="cil-lock-locked" />
+                  </CInputGroupText>
+                  <input
+                    id="password"
+                    v-model="form.password"
+                    type="password"
+                    class="form-control"
+                    required
+                    name="password"
+                  />
+                </CInputGroup>
+                <CRow>
+                  <CCol :xs="5">
+                    <CButton color="primary" @click="login" class="px-4 col"
+                      >Login</CButton
+                    >
+                  </CCol>
+                </CRow>
               </CCardBody>
             </CCard>
           </CCardGroup>
@@ -55,5 +52,29 @@
 <script>
 export default {
   name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: '',
+      form: {
+        username: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    login: function (e) {
+      e.preventDefault()
+      if (this.form.username == '' || this.form.password == '') {
+        alert('lengkapi username atau password')
+      } else {
+        const data = {
+          username: this.form.username,
+          password: this.form.password,
+        }
+        console.log(data)
+      }
+    },
+  },
 }
 </script>
